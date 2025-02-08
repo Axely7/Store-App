69d75867221bd35b9ae3d65e24869cca63a794d6
Axely7 <axely7@hotmail.com>
2025-02-08 16:36:59 -0600

    mutation

--- app/(products-app)/product/[id].tsx
+++ app/(products-app)/product/[id].tsx
@@ -16,11 +16,12 @@ import ProductImages from "@/presentation/products/components/ProductImages";
 import ThemeButtonGroup from "@/presentation/theme/components/ThemedButtonGroup";
 import ThemedButton from "@/presentation/theme/components/ThemedButton";
 import { Formik } from "formik";
+import { Size } from "@/core/products/interfaces/product.interface";
 
 const ProductScreen = () => {
   const navigation = useNavigation();
   const { id } = useLocalSearchParams();
-  const { productQuery } = useProduct(`${id}`);
+  const { productQuery, productMutation } = useProduct(`${id}`);
 
   useEffect(() => {
     navigation.setOptions({
@@ -51,66 +52,95 @@ const ProductScreen = () => {
   const product = productQuery.data!;
 
   return (
-    <Formik initialValues={product}>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : undefined}
-      >
-        <ScrollView>
-          <ProductImages images={product.images} />
-          <ThemedView style={{ marginHorizontal: 10, marginTop: 20 }}>
-            <ThemedTextInput
-              placeholder="Título"
-              style={{ marginVertical: 5 }}
-            />
-            <ThemedTextInput placeholder="Slug" style={{ marginVertical: 5 }} />
-            <ThemedTextInput
-              multiline
-              placeholder="Descripción"
-              numberOfLines={5}
-              style={{ marginVertical: 5 }}
-            />
-          </ThemedView>
-          <ThemedView
-            style={{
-              marginHorizontal: 10,
-              marginVertical: 5,
-              flexDirection: "row",
-              gap: 10,
-            }}
-          >
-            <ThemedTextInput placeholder="Precio" style={{ flex: 1 }} />
-            <ThemedTextInput placeholder="Inventario" style={{ flex: 1 }} />
-          </ThemedView>
-
-          <ThemedView style={{ marginHorizontal: 10 }}>
-            <ThemeButtonGroup
-              options={["XS", "S", "M", "L", "XL", "XXL", "XXXL"]}
-              selectedOptions={product.sizes}
-              onSelect={(options) => console.log({ options })}
-            />
-            <ThemeButtonGroup
-              options={["kid", "men", "women", "unisex"]}
-              selectedOptions={[product.gender]}
-              onSelect={(options) => console.log({ options })}
-            />
-          </ThemedView>
-
-          <View
-            style={{
-              marginHorizontal: 10,
-              marginBottom: 50,
-              marginTop: 20,
-            }}
-          >
-            <ThemedButton
-              icon="save-outline"
-              onPress={() => console.log("guardar")}
+    <Formik initialValues={product} onSubmit={productMutation.mutate}>
+      {({ values, handleSubmit, handleChange, setFieldValue }) => (
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : undefined}
+        >
+          <ScrollView>
+            <ProductImages images={values.images} />
+            <ThemedView style={{ marginHorizontal: 10, marginTop: 20 }}>
+              <ThemedTextInput
+                placeholder="Título"
+                style={{ marginVertical: 5 }}
+                value={values.title}
+                onChangeText={handleChange("title")}
+              />
+              <ThemedTextInput
+                placeholder="Slug"
+                style={{ marginVertical: 5 }}
+                value={values.slug}
+                onChangeText={handleChange("slug")}
+              />
+              <ThemedTextInput
+                multiline
+                placeholder="Descripción"
+                numberOfLines={5}
+                style={{ marginVertical: 5 }}
+                value={values.description}
+                onChangeText={handleChange("description")}
+              />
+            </ThemedView>
+            <ThemedView
+              style={{
+                marginHorizontal: 10,
+                marginVertical: 5,
+                flexDirection: "row",
+                gap: 10,
+              }}
             >
-              Guardar
-            </ThemedButton>
-          </View>
-        </ScrollView>
-      </KeyboardAvoidingView>
+              <ThemedTextInput
+                placeholder="Precio"
+                style={{ flex: 1 }}
+                value={values.price.toString()}
+                onChangeText={handleChange("price")}
+              />
+              <ThemedTextInput
+                placeholder="Inventario"
+                style={{ flex: 1 }}
+                value={values.stock.toString()}
+                onChangeText={handleChange("stock")}
+              />
+            </ThemedView>
+
+            <ThemedView style={{ marginHorizontal: 10 }}>
+              <ThemeButtonGroup
+                options={["XS", "S", "M", "L", "XL", "XXL", "XXXL"]}
+                selectedOptions={values.sizes}
+                onSelect={(selectedSize) => {
+                  if (values.sizes.includes(selectedSize as Size)) {
+                    const newSizes = values.sizes.filter(
+                      (size) => size !== selectedSize
+                    );
+                    setFieldValue("sizes", newSizes);
+                  } else {
+                    setFieldValue("sizes", [...values.sizes, selectedSize]);
+                  }
+                }}
+              />
+              <ThemeButtonGroup
+                options={["kid", "men", "women", "unisex"]}
+                selectedOptions={[values.gender]}
+                onSelect={(selectedOption) =>
+                  setFieldValue("gender", selectedOption)
+                }
+              />
+            </ThemedView>
+
+            <View
+              style={{
+                marginHorizontal: 10,
+                marginBottom: 50,
+                marginTop: 20,
+              }}
+            >
+              <ThemedButton icon="save-outline" onPress={() => handleSubmit()}>
+                Guardar
+              </ThemedButton>
+            </View>
+          </ScrollView>
+        </KeyboardAvoidingView>
+      )}
     </Formik>
   );
 };
--- presentation/products/hooks/useProduct.ts
+++ presentation/products/hooks/useProduct.ts
@@ -1,8 +1,16 @@
-import { useQuery } from '@tanstack/react-query'
-import React from 'react'
+import { useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { getProductById } from '../../../core/products/actions/get-product-by-id.action';
+import { Product } from '../../../core/products/interfaces/product.interface';
+import { Alert } from 'react-native';
+import { updateCreateProduct } from '../../../core/products/actions/create-update-product.action';
+import { useRef } from 'react';
 
 export const useProduct = (productId: string) => {
+
+  const queryClient = useQueryClient();
+  const productIdRef = useRef(productId);
+
     
     const productQuery = useQuery({
         queryKey: ['product', productId],
@@ -10,8 +18,32 @@ export const useProduct = (productId: string) => {
         staleTime: 1000 * 60 * 60 // 1 hora
     })
 
+    const productMutation = useMutation({
+      mutationFn: async (data: Product) => updateCreateProduct({
+        ...data,
+        id: productIdRef.current
+      }),
+      onSuccess: (data: Product) => {
+
+        productIdRef.current = data.id
+
+
+        queryClient.invalidateQueries({
+          queryKey: ['products', 'infinite']
+        });
+
+        queryClient.invalidateQueries({
+          queryKey: [ 'product', data.id]
+        });
+
+
+        Alert.alert('Producto guardado', `${data.title} se guardó correctamente`)
+      }
+    })
+
 
   return {
-    productQuery
+    productQuery,
+    productMutation
   }
 }
